510e4bdbe934cf4f3f8a7071d746fe0710e7e0fa
haykal-fe-verd <haykal@verd.net.id>
2024-06-17 23:32:45 +0700

    fix: fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        hmr: {
-            host: "localhost",
-            clientPort: 5173,
-            protocol: "ws",
-        },
-        port: 5173,
-        watch: {
-            usePolling: true,
-        },
-    },
+    // server: {
+    //     host: "0.0.0.0",
+    //     hmr: {
+    //         host: "localhost",
+    //         clientPort: 5173,
+    //         protocol: "ws",
+    //     },
+    //     port: 5173,
+    //     watch: {
+    //         usePolling: true,
+    //     },
+    // },
     plugins: [
         laravel({
             input: "resources/js/app.tsx",
